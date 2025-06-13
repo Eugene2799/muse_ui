f693964618f216d81a3d8695c3ada65fc85ac16e
Muse Tools <muse-tools@outlook.com>
2025-06-13 17:29:34 +0800

    update web demo for V1.0.1+1.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "588880156"
+    serviceWorkerVersion: "2332902805"
   }
 });
